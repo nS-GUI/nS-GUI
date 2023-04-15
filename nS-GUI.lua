9431e7acd1edea4bd27a635e153d4a7edae160e3
nS-GUI <130631477+nS-GUI@users.noreply.github.com>
2023-04-15 12:56:35 -0300

    Update nS-GUI.lua

--- nS-GUI.lua
+++ nS-GUI.lua
@@ -1,1 +1,97 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "nS・GUI  |  Key System", HidePremium = false, IntroText = "nS-GUI | Carregando...", SaveConfig = true, ConfigFolder = "nS-FILE-KEY"})
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Grayy12/KeySys/main/Protected%20(4).lua",true))()
+
+OrionLib:MakeNotification({
+	Name = "Discord:",
+	Content = "discord.gg/invite",
+	Image = "rbxassetid://13132648697",
+	Time = 25
+})
+
+_G.Key = "nScripted"
+_G.KeyInput = "string"
+
+function CheckKey()
+OrionLib:MakeNotification({
+	Name = "Sucesso!",
+	Content = "Você ejetou o script.",
+	Image = "rbxassetid://13132648697",
+	Time = 25
+})
+
+
+      OrionLib:Destroy()
+      wait(2)
+local startTime = os.clock()
+while os.clock() - startTime < 5 do
+    loadingLabel.Text = "nS・GUI | Loading."
+    wait(0.2)
+    loadingLabel.Text = "nS・GUI | Loading.."
+    wait(0.2)
+    loadingLabel.Text = "nS・GUI | Loading..."
+    wait(0.2)
+    end
+    wait(1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nS-GUI/nS-GUI/main/nS-GUI-ORION.lua"))()
+    end
+
+function incorreto()
+  OrionLib:MakeNotification({
+	Name = "Key Incorreta",
+	Content = "Você usou a key errada.",
+	Image = "rbxassetid://13132648697",
+	Time = 25
+})
+  end
+
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+
+
+
+
+
+MainTab:AddTextbox({
+	Name = "Enter Key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		_G.KeyInput = Value
+	end	  
+})
+
+--[[
+Name = <string> - The name of the textbox.
+Default = <string> - The default value of the textbox.
+TextDisappear = <bool> - Makes the text disappear in the textbox after losing focus.
+Callback = <function> - The function of the textbox.
+]]
+
+
+
+Tab:AddButton({
+	Name = "Login",
+	Callback = function()
+      		if _G.KeyInput == _G.Key then
+          CheckKey()
+          else
+              incorreto()
+        end
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
